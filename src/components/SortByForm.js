69d2d0846de55405b3f51405634ec67e14929a47
Cameron Corrado <camcorrado@gmail.com>
2020-09-01 00:19:27 -0400

    updated favicon and title

--- src/components/SortByForm.js
+++ src/components/SortByForm.js
@@ -1,40 +1,40 @@
-import ApiContext from '../ApiContext'
-import React from 'react'
+import ApiContext from "../ApiContext";
+import React from "react";
 
 export default class YourSuggestionsPage extends React.Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            value: 'newest'
-        }
+    this.state = {
+      value: "newest",
+    };
 
-        this.handleChange = this.handleChange.bind(this)
-    }
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-    static contextType = ApiContext
+  static contextType = ApiContext;
 
-    static defaultProps ={
-        handleSortBy: () => {}
-    }
+  static defaultProps = {
+    handleSortBy: () => {},
+  };
 
-    async handleChange(event) {
-        await this.setState({value: event.target.value})
-        this.context.handleSortBy(this.state.value)
-    }
+  async handleChange(event) {
+    await this.setState({ value: event.target.value });
+    this.context.handleSortBy(this.state.value);
+  }
 
-    render() {
-        return(
-            <form>
-                <label>
-                    Sort Suggestions By:
-                    <select value={this.state.value} onChange={this.handleChange}>
-                        <option value='newest'>Newest</option>
-                        <option value='oldest'>Oldest</option>
-                        <option value='upvotes'>Upvotes</option>
-                    </select>
-                </label>
-            </form>
-        )
-    }
+  render() {
+    return (
+      <form>
+        <label>
+          Sort Suggestions By:
+          <select value={this.state.value} onChange={this.handleChange}>
+            <option value="newest">Newest</option>
+            <option value="oldest">Oldest</option>
+            <option value="upvotes">Upvotes</option>
+          </select>
+        </label>
+      </form>
+    );
+  }
 }
